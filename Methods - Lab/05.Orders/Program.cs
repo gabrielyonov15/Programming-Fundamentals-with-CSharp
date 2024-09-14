00e984db33ad62b7cbb0eb378f8b1176811ded27
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:24:46 +0300

    Add files via upload

---
+++ Methods - Lab/05.Orders/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            CalculateTotalPrice(product, quantity);
+        }
+        static void CalculateTotalPrice(string product, int quantity)
+        {
+            double price = 0.0;
+
+            switch (product)
+            {
+                case "coffee":
+                    price = 1.50;
+                    break;
+                case "water":
+                    price = 1.00;
+                    break;
+                case "coke":
+                    price = 1.40;
+                    break;
+                case "snacks":
+                    price = 2.00;
+                    break;
+            }
+            double totalPrice = price * quantity;
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
---
+++ Methods - Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int area = CalculateRectangleArea(width, height);
+            Console.WriteLine(area);
+        }
+        static int CalculateRectangleArea(int width, int height)
+        {
+            return width * height;
+        }
+    }
+}
