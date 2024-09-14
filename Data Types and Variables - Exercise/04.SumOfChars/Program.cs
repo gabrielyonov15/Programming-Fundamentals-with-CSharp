fd779e4b182807ee2e3b0cb3b5721dc047637533
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:09:56 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/04.SumOfChars/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                char symbols = char.Parse(Console.ReadLine());
+                totalSum += (int)symbols;
+            }
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (char first = 'a'; first < 'a' + n; first++)
+            {
+                for (char second = 'a'; second < 'a' + n; second++)
+                {
+                    for (char third = 'a'; third < 'a' + n; third++)
+                    {
+                        Console.WriteLine($"{first}{second}{third}");
+                    }
+                }
+            }
+        }
+    }
+}
