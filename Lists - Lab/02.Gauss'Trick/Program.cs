8475ab1f29c481df3c72294c99424dd5ae8d79f8
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 15:01:12 +0300

    Add files via upload

---
+++ Lists - Lab/02.Gauss'Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.Gauss_Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputList = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+            List<int> newList = new List<int>();
+            int i = 0;
+            while (i < inputList.Count / 2)
+            {
+                int sum = inputList[i] + inputList[inputList.Count - 1 - i];
+                newList.Add(sum);
+                i++;
+            }
+            if (inputList.Count % 2 != 0)
+            {
+                newList.Add(inputList[inputList.Count / 2]);
+            }
+            Console.WriteLine(string.Join(" ", newList));
+        }
+    }
+}
---
+++ Lists - Lab/03.MergingLists/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+            List<int> secondList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> resultList = new List<int>();
+            int minLength = Math.Min(firstList.Count, secondList.Count);
+            for (int i = 0; i < minLength; i++)
+            {
+                resultList.Add(firstList[i]);
+                resultList.Add(secondList[i]);
+            }
+            for (int i = minLength; i < firstList.Count; i++)
+            {
+                resultList.Add(firstList[i]);
+            }
+            for (int i = minLength; i < secondList.Count; i++)
+            {
+                resultList.Add(secondList[i]);
+            }
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
