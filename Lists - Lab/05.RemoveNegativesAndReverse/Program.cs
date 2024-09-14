0af9d9551d34ebe0af006492de942c97e2bcb8b2
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 15:02:28 +0300

    Add files via upload

---
+++ Lists - Lab/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> reversedNumbers = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                int number = numbers[i];
+                reversedNumbers.Add(number);
+            }
+            if (reversedNumbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", reversedNumbers));
+            }
+        }
+    }
+}
---
+++ Lists - Lab/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _06.ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+              .Split()
+              .Select(int.Parse)
+              .ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                string[] commandParts = command.Split();
+                string action = commandParts[0];
+                switch (action)
+                {
+                    case "Add":
+                        int numberToAdd = int.Parse(commandParts[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+                    case "Remove":
+                        int numberToRemove = int.Parse(commandParts[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int indexToRemove = int.Parse(commandParts[1]);
+                        numbers.RemoveAt(indexToRemove);
+                        break;
+                    case "Insert":
+                        int numberToInsert = int.Parse(commandParts[1]);
+                        int indexToInsert = int.Parse(commandParts[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
