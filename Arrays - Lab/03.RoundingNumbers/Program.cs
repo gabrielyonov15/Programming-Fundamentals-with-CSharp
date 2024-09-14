d939c8c8dad57b32024dd34f3e083037ec9764a3
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:14:23 +0300

    Add files via upload

---
+++ Arrays - Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+
+namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] number =Console.ReadLine().Split().Select(double.Parse).ToArray();
+            int[] roundedNums = new int[number.Length];
+            for (int i = 0; i < number.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(number[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{number[i]} => {roundedNums[i]}");
+            }
+        }
+    }
+}
