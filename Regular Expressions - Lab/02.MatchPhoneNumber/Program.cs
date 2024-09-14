f4410df0a7325615ce82655de92b32a6a213def7
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 15:30:19 +0300

    Add files via upload

---
+++ Regular Expressions - Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            var validPhones = matches
+                .Cast<Match>()
+                .Select(match => match.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(", ", validPhones));
+        }
+    }
+}
---
+++ Regular Expressions - Lab/03.MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b(?<day>\d{2})(?<separator>[.\-/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>\d{4})\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                var day = match.Groups["day"].Value;
+                var separator = match.Groups["separator"].Value;
+                var month = match.Groups["month"].Value;
+                var year = match.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
