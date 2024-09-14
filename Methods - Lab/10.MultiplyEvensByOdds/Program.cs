4967c53a549b9451dd2051b82de6a02259f2c12e
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:27:25 +0300

    Add files via upload

---
+++ Methods - Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+            }
+            static int GetMultipleOfEvenAndOdds(int number)
+            {
+                int evenSum = GetSumOfEvenDigits(number);
+                int oddSum = GetSumOfOddDigits(number);
+                return evenSum * oddSum;
+            }
+            static int GetSumOfEvenDigits(int number)
+            {
+                int sum = 0;
+                while (number != 0)
+                {
+                    int digit = Math.Abs(number % 10);
+                    if (digit % 2 == 0)
+                    {
+                        sum += digit;
+                    }
+                    number /= 10;
+                }
+                return sum;
+            }
+            static int GetSumOfOddDigits(int number)
+            {
+                int sum = 0;
+                while (number != 0)
+                {
+                    int digit = Math.Abs(number % 10);
+                    if (digit % 2 != 0)
+                    {
+                        sum += digit;
+                    }
+                    number /= 10;
+                }
+                return sum;
+            }
+        }
+    }
---
+++ Methods - Lab/11.MathOperations/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double result = PerformOperation(firstNumber, operation, secondNumber);
+            Console.WriteLine(result);
+        }
+        static double PerformOperation(double firstNumber, char operation, double secondNumber)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                    }
+                    break;
+            }
+            return result;
+        }
+    }
+}
