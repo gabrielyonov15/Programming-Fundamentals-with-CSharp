5505dbb1b3b86eaf9df4d14ae283a36b125691d6
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 15:06:21 +0300

    Add files via upload

---
+++ Lists - Exercise/04.ListOperations/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _04.ListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+                string[] commandParts = command.Split();
+                string action = commandParts[0];
+                if (action == "Add")
+                {
+                    int numberToAdd = int.Parse(commandParts[1]);
+                    numbers.Add(numberToAdd);
+                }
+                else 
+                if (action == "Insert")
+                {
+                    int numberToInsert = int.Parse(commandParts[1]);
+                    int index = int.Parse(commandParts[2]);
+                    if (index < 0 || index >= numbers.Count)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        numbers.Insert(index, numberToInsert);
+                    }
+                }
+                else 
+                if (action == "Remove")
+                {
+                    int index = int.Parse(commandParts[1]);
+                    if (index < 0 || index >= numbers.Count)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        numbers.RemoveAt(index);
+                    }
+                }
+                else 
+                if (action == "Shift")
+                {
+                    string direction = commandParts[1];
+                    int count = int.Parse(commandParts[2]);
+                    if (direction == "left")
+                    {
+                        ShiftLeft(numbers, count);
+                    }
+                    else 
+                    if (direction == "right")
+                    {
+                        ShiftRight(numbers, count);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+        static void ShiftLeft(List<int> list, int count)
+        {
+            int actualCount = count % list.Count;
+            for (int i = 0; i < actualCount; i++)
+            {
+                int firstElement = list[0];
+                list.RemoveAt(0);
+                list.Add(firstElement);
+            }
+        }
+        static void ShiftRight(List<int> list, int count)
+        {
+            int actualCount = count % list.Count;
+            for (int i = 0; i < actualCount; i++)
+            {
+                int lastElement = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                list.Insert(0, lastElement);
+            }
+        }
+    }
+}
---
+++ Lists - Exercise/06.CardsGame/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _06.CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayerHand = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondPlayerHand = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (firstPlayerHand.Count > 0 && secondPlayerHand.Count > 0)
+            {
+                int firstCard = firstPlayerHand[0];
+                int secondCard = secondPlayerHand[0];
+                firstPlayerHand.RemoveAt(0);
+                secondPlayerHand.RemoveAt(0);
+                if (firstCard > secondCard)
+                {
+                    firstPlayerHand.Add(firstCard);
+                    firstPlayerHand.Add(secondCard);
+                }
+                else
+                if (secondCard > firstCard)
+                {
+                    secondPlayerHand.Add(secondCard);
+                    secondPlayerHand.Add(firstCard);
+                }
+            }
+            if (firstPlayerHand.Count > 0)
+            {
+                int sum = firstPlayerHand.Sum();
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+            else
+            {
+                int sum = secondPlayerHand.Sum();
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }
+        }
+    }
+}
