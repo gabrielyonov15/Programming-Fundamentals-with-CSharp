c0ff55cfe48cdaa4a57d215d687d5dd6d176858f
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:06:54 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/10.LowerOrUpper/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10.LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char whether = char.Parse(Console.ReadLine());
+            if(whether >= 'A' && whether <= 'Z')
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            if (whether >= 'a' && whether <= 'z')
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
---
+++ Data Types and Variables - Lab/11.RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _11.RefactorVolumeOfPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double lengt = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double  width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+            double volume = (lengt * width * height) /3;
+            Console.Write($"Pyramid Volume: {volume:f2}");
+        }
+    }
+}
