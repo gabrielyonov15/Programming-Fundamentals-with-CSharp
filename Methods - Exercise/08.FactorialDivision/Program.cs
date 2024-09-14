cbbea14600fdd848f65d0c89dfcacecee04f8496
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:58:27 +0300

    Add files via upload

---
+++ Methods - Exercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{FactorialDivision(a, b):f2}");
+        }
+        static double Factorial(int n)
+        {
+            double result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        static double FactorialDivision(int a, int b)
+        {
+            double factorialA = Factorial(a);
+            double factorialB = Factorial(b);
+            double result = factorialA / factorialB;
+            return Math.Round(result, 2);
+        }
+    }
+}
---
+++ Methods - Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                int number = int.Parse(input);
+                bool isPalindrome = IsPalindrome(number);
+                if (isPalindrome)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+        static bool IsPalindrome(int number)
+        {
+            string numberString = number.ToString();
+            int left = 0;
+            int right = numberString.Length - 1;
+            while (left < right)
+            {
+                if (numberString[left] != numberString[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
