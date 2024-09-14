81048a43ba106b4147e1907f44649a5a02494a03
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 13:49:03 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/04.BackIn30Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04.BackIn30Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours += 1;
+            }
+            if (hours >= 24)
+            {
+                hours -= 24;
+            }
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
+
