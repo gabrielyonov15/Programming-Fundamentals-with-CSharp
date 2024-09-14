06c5563bcf9e53567f8c116b08ea789fced890cb
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:16:43 +0300

    Add files via upload

---
+++ Arrays - Lab/07.EqualArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            bool arraysAreIdentical = true;
+            for (int i = 0;i<firstNumber.Length;i++)
+            {
+                if (firstNumber[i] == secondNumber[i])
+                {
+                    sum += firstNumber[i];
+                }
+                else
+                if (firstNumber[i] != secondNumber[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    arraysAreIdentical = false;
+                    break;
+                }
+            }
+            if(arraysAreIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
