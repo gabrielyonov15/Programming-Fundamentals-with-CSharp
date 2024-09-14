c219a4516f76e5632ab5e1463f74a5a60a10b3ac
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 15:16:12 +0300

    Add files via upload

---
+++ Objects and Classes - Exercise/07.OrderByAge/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _07.OrderByAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, (string name, int age)> people = new Dictionary<string, (string name, int age)>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split();
+                string name = tokens[0];
+                string id = tokens[1];
+                int age = int.Parse(tokens[2]);
+                if (people.ContainsKey(id))
+                {
+                    people[id] = (name, age);
+                }
+                else
+                {
+                    people.Add(id, (name, age));
+                }
+            }
+            foreach (var person in people.OrderBy(p => p.Value.age))
+            {
+                Console.WriteLine($"{person.Value.name} with ID: {person.Key} is {person.Value.age} years old.");
+            }
+        }
+    }
+}
