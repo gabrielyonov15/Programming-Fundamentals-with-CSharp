15dd135d597183cd090b7b9ff78652f7af5134b5
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 13:57:54 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/05.Login/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string username = Console.ReadLine();
+                string reversedUsername = new string(username.Reverse().ToArray());
+                bool loggedIn = false; 
+                string password = Console.ReadLine();
+                if (password == reversedUsername)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    loggedIn = true;
+                }
+                else
+                {
+                    for (int i = 1; i < 4; i++)
+                    {
+                        Console.WriteLine("Incorrect password. Try again.");
+                        password = Console.ReadLine();
+                        if (password == reversedUsername)
+                        {
+                            Console.WriteLine($"User {username} logged in.");
+                            loggedIn = true; 
+                            break;
+                        }
+                    }
+                }
+                if (!loggedIn) 
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                }
+            }
+        }
+    }
+  
