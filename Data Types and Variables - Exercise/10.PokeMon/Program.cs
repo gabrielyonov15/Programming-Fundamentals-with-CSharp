0edba731abc05fc09b080345e704eeae31507450
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:12:24 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/10.PokeMon/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _10.PokeMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            int originalN = N;
+            int targetsPoked = 0;
+            while (N >= M)
+            {
+                N -= M;
+                targetsPoked++;
+                if (N == originalN / 2 && Y != 0)
+                {
+                    N /= Y;
+                }
+            }
+            Console.WriteLine(N);
+            Console.WriteLine(targetsPoked);
+        }
+    }
+}
