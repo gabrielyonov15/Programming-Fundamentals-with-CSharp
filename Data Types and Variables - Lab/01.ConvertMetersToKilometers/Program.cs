3dbbb1f5a72c35a69d4958a050f59c282eef4c4b
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:03:07 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01.ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = n / 1000m;
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
---
+++ Data Types and Variables - Lab/02.PoundsToDollars/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02.PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double sum = n * 1.31;
+            Console.WriteLine($"{sum:f3}");
+        }
+    }
+}
