95fea13fdfd17ae2b50e7db7328f647a3a630256
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 15:07:45 +0300

    Add files via upload

---
+++ Lists - Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arrays = input.Split('|').Reverse().ToArray();
+            List<int> result = new List<int>();
+            foreach (string array in arrays)
+            {
+                int[] numbers = array
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                result.AddRange(numbers);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
