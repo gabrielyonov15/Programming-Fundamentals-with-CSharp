8f49d99e2cb32655e4506f32d752419b24735936
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:23:26 +0300

    Add files via upload

---
+++ Methods - Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            CheckNumber(n);
+        }
+        static void CheckNumber(int number)
+        {
+            if(number >0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+                if(number <0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
