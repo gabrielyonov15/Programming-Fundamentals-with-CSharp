a2317da04a76dee619b5bf729c094a430cc1327a
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 13:59:24 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace _6StrongNu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int originalNumber = number;
+            int sumOfFactorials = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                int factorial = 1;
+                for (int i = 2; i <= digit; i++)
+                {
+                    factorial *= i;
+                }
+                sumOfFactorials += factorial;
+                number /= 10;
+            }
+            if (sumOfFactorials == originalNumber)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
