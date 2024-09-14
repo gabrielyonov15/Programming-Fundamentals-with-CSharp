d3e4eee53abc4106577891d0a99d9bfe9291953a
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 14:26:21 +0300

    Add files via upload

---
+++ Methods - Lab/07.RepeatString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int repeatCount = int.Parse(Console.ReadLine());
+            string result = RepeatString(inputString, repeatCount);
+            Console.WriteLine(result);
+        }
+        static string RepeatString(string str, int n)
+        {
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                result += str;
+            }
+            return result;
+        }
+    }
+}
---
+++ Methods - Lab/08.MathPower/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = MathPower(baseNumber, power);
+            Console.WriteLine(result);
+        }
+        static double MathPower(double baseNumber, int power)
+        {
+            return Math.Pow(baseNumber, power);
+        }
+    }
+}
+
